814d991db1350be3c73cab50d4fa09f0d7abac2f
Josh Reisner <josh@joshreisner.com>
2015-10-29 08:58:46 -0700

    importer

--- functions.php
+++ functions.php
@@ -24,19 +24,26 @@ add_action('pre_get_posts', function($wp_query){
 
 function format_cell($cell) {
 	if ($cell == 'NULL') return '';
-	return trim(str_replace('"', '', $cell));
+	$cell = str_replace('*', '', str_replace('"', '', str_replace('.', '', $cell)));
+	return trim($cell);
 }
 
-function format_name($name) {
-	$name = str_replace('.', ' . ', $name);
-	$name = str_replace('-', ' - ', $name);
-	$name = str_replace('(', ' ( ', $name);
-	$name = str_replace(')', ' ) ', $name);
-	$name = ucwords(strtolower($name));
-	$words = explode(' ', $name);
+function format_day($day) {
+	$days = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+	if (!in_array($day, $days)) die('Error: day "' . $day . '" was not found!');
+	return $day;
+}
+
+function title_case($string) {
+	$string = str_replace('.', ' . ', $string);
+	$string = str_replace('-', ' - ', $string);
+	$string = str_replace('/', ' / ', $string);
+	$string = str_replace('(', ' ( ', $string);
+	$string = ucwords(strtolower($string));
+	$words = explode(' ', $string);
 	$count = count($words);
-	$lower = array('a', 'and', 'at', 'de', 'for', 'in', 'la', 'los', 'of', 'on', 'or', 'the', 'to', 'with');
-	$upper = array('AA', 'LGBT', 'PM', 'SOS');
+	$lower = array('a', 'and', 'at', 'but', 'by', 'de', 'for', 'in', 'la', 'los', 'nor', 'of', 'on', 'or', 'the', 'to', 'with');
+	$upper = array('AA', 'LGBT', 'NY', 'NYC', 'NYU', 'PM', 'SOS', 'YMCA', 'YWCA');
 	for ($i = 0; $i < $count; $i++) {
 		if ($i && ($i != $count - 1) && in_array(strtolower($words[$i]), $lower)) {
 			$words[$i] = strtolower($words[$i]);
@@ -44,18 +51,94 @@ function format_name($name) {
 			$words[$i] = strtoupper($words[$i]);
 		}
 	}
-	$name = implode(' ', $words);
-	while (strstr($name, ' .')) $name = str_replace(' .', '.', $name);
-	while (strstr($name, ' - ')) $name = str_replace(' - ', '-', $name);
-	while (strstr($name, '( ')) $name = str_replace('( ', '(', $name);
-	while (strstr($name, ' )')) $name = str_replace(' )', ')', $name);
-	while (strstr($name, '  ')) $name = str_replace('  ', ' ', $name);
+	$string = implode(' ', $words);
+	while (strstr($string, ' .'))  $string = str_replace(' .',  '.', $string);
+	while (strstr($string, ' -')) $string = str_replace(' -', '-', $string);
+	while (strstr($string, '- ')) $string = str_replace('- ', '-', $string);
+	while (strstr($string, ' /')) $string = str_replace(' /', '/', $string);
+	while (strstr($string, '/ ')) $string = str_replace('/ ', '/', $string);
+	while (strstr($string, '( '))  $string = str_replace('( ',  '(', $string);
+	while (strstr($string, '  '))  $string = str_replace('  ',  ' ', $string);
+	if ($string == 'Sage') return 'S.A.G.E.';
+	if ($string == 'Home') return 'H.O.P.E.';
+	if ($string == 'Byoc') return 'B.Y.O.C.';
+	if ($string == 'Now-No Other Way') return 'N.O.W.-No Other Way';
+	if ($string == 'How Club') return 'H.O.W. Club';
+	
+	return $string;
+}
+
+function format_name($name) {
+	list($name, $after) = explode('(', $name, 2);
+	$name = title_case($name);
 	return $name;
 }
 
-function format_location($name) {
-	$name = format_name($name);
-	return $name;
+function format_location($location) {
+	$location = title_case($location);
+	return $location;
+}
+
+function format_address(&$row) {
+	$row['address'] = title_case($row['address']);
+	
+	//move everything after the comma to the notes
+	list($row['address'], $after) = explode('(', $row['address'], 2);
+	if ($after) $row['notes'] = str_replace(')', '', $after) . '<br>' . $row['notes'];
+	
+	list($row['address'], $after) = explode(',', $row['address'], 2);
+	if ($after) $row['notes'] = $after . '<br>' . $row['notes'];
+	
+	//remove everything after @
+	list($row['address'], $after) = explode('@', $row['address'], 2);
+
+	//remove everything after &
+	list($row['address'], $after) = explode('&', $row['address'], 2);
+
+	$row['address']	= trim($row['address']);
+	
+	if (empty($row['address'])) die(implode(', ', $row));
+}
+
+function format_city($row) {
+	extract($row);
+	$cities = array('Hoboken', 'Yonkers', 'Saltaire', 'Kingston', 'Orangeburg', 'Mt Vernon', 'Callicoon',
+		'White Plains', 'New Rochelle', 'Ocean Beach', 'Harrison', 'Scarsdale', 'Middletown', 'Wesley Hills',
+		'Yorktown Heights', 'Brewster', 'Bellmore', 'Port Chester', 'Norwalk', 'Monroe', 'Stony Point', 
+		'Larchmont', 'Purchase', 'Poughkeepsie', 'Katonah', 'Patchogue', 'Warwick', 'Newburgh', 'Dobbs Ferry',
+		'Ferndale', 'Pearl River', 'Montauk', 'Shelter Island', 'Montrose', 'Valley Cottage', 'Sag Harbor',
+		'Nyack', 'Blairstown', 'Harriman', 'Tarrytown', 'Port Washington', 'Cornwall', 'Mahopac', 'Babylon',
+		'New Windsor', 'Cold Spring', 'Wappingers Falls', 'Briarcliff Manor', 'Pelham', 'Peekskill', 'Airmont',
+		'Kent', 'Melville', 'Hewlett', 'Mount Sinai', 'Port Jervis', 'Carmel', 'Greenwich', 'Fire Island',
+		'Southampton', 'Bedford', 'Somers', 'Highland Mills', 'Salisbury', 'Pleasantville', );
+	if (empty($city)) {
+		if ($region == 'Bronx') return 'Bronx';
+		if ($region == 'Brooklyn') return 'Brooklyn';
+		if ($region == 'Manhattan') return 'New York';
+		if ($region == 'Queens') return 'Queens';
+		if ($region == 'Staten Island') return 'Staten Island';
+		foreach ($cities as $city) {
+			$string = implode('|', compact('name', 'location', 'address', 'region'));
+			if (stristr($string, $city)) {
+				return $city;
+			}
+		}
+		die('Error: city not found for row ' . $string);
+	} elseif ($city == 'NY') {
+		return 'New York';
+	}
+	return $city;
+}
+
+function format_state($state, $region) {
+	if (empty($state)) {
+		if ($region == 'CT') return 'CT';
+		if ($region == 'NJ') return 'NJ';
+		return 'NY';
+	}
+	$states = array('CT', 'NJ', 'NY');
+	if (!in_array($state, $states)) die('Error: state "' . $state . '" was not found!');
+	return $state;
 }
 
 function format_region($region) {
@@ -79,21 +162,67 @@ function format_region($region) {
 	return $regions[$region];
 }
 
-function format_state($state, $region) {
-	if (empty($state)) {
-		if ($region == 'CT') return 'CT';
-		if ($region == 'NJ') return 'NJ';
-		return 'NY';
+function format_types($types) {
+	$translations = array(
+		'AA Grapevine Literature' => 'Grapevine',
+		'AA Literature' => 'Literature',
+		'Agnostic' => 'Atheist / Agnostic',
+		'As Bill Sees It' => 'Literature',
+		'B = Beginners meeting' => 'Beginner',
+		'BB = Big Book meeting' => 'Big Book',
+		'Beginners Workshop' => 'Beginner',
+		'Big Book Topic' => 'Big Book',
+		'Big Book Workshop' => 'Big Book',
+		'C = Closed Discussion meeting' => 'Closed',
+		'Came To Believe' => 'Literature',
+		//Children Welcome
+		'Daily Reflections' => 'Literature',
+		'Eleventh Step' => 'Step Meeting',
+		'Eleventh Step Meditation' => 'Step Meeting',
+		'First Step Workshop' => 'Step Meeting',
+		'Fourth Step Workshop' => 'Step Meeting',
+		'Gay Men' => 'Gay',
+		'Gay, Lesbian and Bisexual' => 'LGBTQ',
+		//H.I.V Positive
+		'Interpreted for the Deaf' => 'Sign Language',
+		'Lesbian' => 'Lesbian',
+		'Living Sober' => 'Literature',
+		//Medication
+		'Meditation' => 'Meditation',
+		'Meditation at Meeting' => 'Meditation',
+		'Men' => 'Men Only',
+		//Mental Health Issues in Sobriety
+		'O = Open meeting' => 'Open',
+		//OD = Open Discussion meeting
+		//Polish Speaking
+		//Promises
+		'Rotating Step' => 'Step Meeting',
+		//Round-Robin Meeting Format
+		//Russian Speaking
+		'S = Step meeting' => 'Step Meeting',
+		'Sp = Spanish speaking group' => 'Spanish',
+		'Spanish Speaking' => 'Spanish',
+		//Special Purpose Groups
+		//Spiritual Workshop
+		//Sponsorship Workshop
+		'Steps 1-2-3' => 'Step Meeting',
+		'T = Tradition meeting' => 'Tradition',
+		'Third Step' => 'Step Meeting',
+		'Topic' => 'Topic Discussion',
+		'Twelve Steps' => 'Step Meeting',
+		'Twelve Traditions' => 'Tradition',
+		//Under Six Months Sober
+		'Women' => 'Women Only',
+		'Young People' => 'Young People',
+	);
+	$return = array();
+	$types = array_map('trim', explode('<br>', $types));
+	foreach ($types as $type) {
+		if (array_key_exists($type, $translations)) {
+			$return[] = $translations[$type];
+		}
 	}
-	$states = array('CT', 'NJ', 'NY');
-	if (!in_array($state, $states)) die('Error: state "' . $state . '" was not found!');
-	return $state;
-}
-
-function format_day($day) {
-	$days = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-	if (!in_array($day, $days)) die('Error: day "' . $day . '" was not found!');
-	return $day;
+	return implode(', ', array_unique($return));
 }
 
 function format_notes($notes) {
--- page-import.php
+++ page-import.php
@@ -1,25 +1,172 @@
 <?php
 
-
-$file = file_get_contents(__DIR__ . '/import.txt');
-
-$rows = explode("\r", $file);
-
-$columns = array_map('sanitize_title', explode("\t", array_shift($rows)));
-
-$meetings = array();
+$time_start = microtime(true);
+$newline	= "\r";
+$tab		= "\t";
+$file		= file_get_contents(__DIR__ . '/import.txt');
+$rows		= explode($newline, $file);
+$columns	= array_map('sanitize_title', explode($tab, array_shift($rows)));
+$meetings	= array();
 
 foreach ($rows as $row) {
-	$row = array_combine($columns, array_map('format_cell', explode("\t", $row)));
+	$row = array_combine($columns, array_map('format_cell', explode($tab, $row)));
 	if (empty($row['day'])) continue;
+	if (strstr($row['location'], 'GROUP LOST ITS MEETING SPACE')) continue;
+	if ($row['name'] == 'QOUGUE BELOW THE BAR') continue; //http://www.27east.com/news/article.cfm/Quogue/126460/Demolition-Crews-Knock-Down-Old-VFW-Post-In-Quogue
+	
+	
+	//manual replacements
+	if ($row['name'] == 'THANKS GIVING GROUP') {
+		$row['city'] = 'Bronxville';
+	} elseif ($row['location'] == 'Brick Church Parish Hall') {
+		$row['city'] = 'Spring Valley';
+	} elseif ($row['address'] == '2021 Albany Post Road') {
+		$row['city'] = 'Croton-On-Hudson';
+	} elseif ($row['name'] == 'CRESTWOOD GARDENS') {
+		$row['city'] = 'Yonkers';
+	} elseif ($row['location'] == 'Temple Kol-Ami') {
+		$row['city'] = 'White Plains';
+	} elseif ($row['address'] == '137 North Division Street') {
+		$row['city'] = 'Peekskill';
+	} elseif ($row['location'] == 'Holy Infant Church') {
+		$row['address'] = '450 Racebrook Road';
+		$row['city'] = 'Orange';
+	} elseif ($row['address'] == '502 West165th Street, Basement') {
+		$row['address'] = '502 West 165th Street, Basement';
+	} elseif (strstr($row['location'], 'Graymoor')) {
+		$row['location'] = 'Graymoor Spiritual Life Center';
+		$row['address'] = '1320 Route 9, 4th Floor Conference Room';
+	} elseif ($row['location'] == 'Helen Hayes Hospital Annex') {
+		$row['address'] = '51-55 Route 9W North West';
+		$row['city'] = 'Haverstraw';
+	} elseif ($row['address'] == '7 East 10th Strert') {
+		$row['address'] = '7 East 10th Street';
+	} elseif ($row['address'] == 'Routes 100 & 202, Basement') {
+		$row['address'] = '331 NY-100, Basement';
+		$row['state'] = 'NY';
+	} elseif ($row['location'] == 'St. Albans Veterans Hosp') {
+		$row['location'] = 'St. Albans Veterans Hospital';
+	} elseif ($row['name'] == 'EXCHANGE VIEWS @ ST MARGARET\'S HOUSE') {
+		$row['name'] = 'Exchange Views';
+		$row['location'] = 'St. Margaret\'s House';		
+	} elseif ($row['address'] == '1170 McLeister Street') {
+		$row['address'] = '1170 McLester St';
+		$row['city'] = 'Elizabeth';
+	} elseif (strstr($row['address'], '74 East 17th Street')) {
+		$row['location'] = 'Old Park Slope Caton';
+	} elseif ($row['address'] == '220 West Houston Street, 2nd Floor') {
+		$row['location'] = 'Midnite';
+	} elseif ($row['address'] == '411 East 12th Street') {
+		$row['location'] = 'The 12th Street Workshop';
+	} elseif ($row['address'] == '50 Perry Street, Ground Floor') {
+		$row['location'] = 'Perry Street Workshop';
+	} elseif ($row['address'] == '1285 Fulton Avenue') {
+		$row['location'] = 'Life Recovery Center';
+	} elseif ($row['address'] == '255 Avenue W') {
+		$row['location'] = 'Safe Haven';
+	} elseif (strstr($row['address'], '122 East 37th Street')) {
+		$row['location'] = 'Mustard Seed';
+	} elseif (strstr($row['address'], '303 West 42nd Street')) {
+		$row['location'] = 'Alanon House';
+	} elseif ($row['address'] == '38-21 99th Street') {
+		$row['location'] = 'Grupo Honestidad';
+	} elseif ($row['address'] == '411 East 12th Street, Basement') {
+		$row['location'] = 'The 12th Street Workshop';
+	} elseif ($row['location'] == 'St Mary\'s Hospital') {
+		//not 100% sure about this
+		$row['location'] = 'Hoboken University Medical Center';
+		$row['address'] = '308 Willow Ave';
+	} elseif ($row['location'] == 'Cherry Grove Fire House') {
+		$row['address'] = '181 Bayview Walk';
+	} elseif ($row['location'] == 'Cornwall Hospital') {
+		//not 100% sure about this
+		$row['location'] = 'St Luke\'s Cornwall Hospital';
+		$row['address'] = '70 Dubois St';
+		$row['city'] = 'Newburgh';
+	} elseif ($row['address'] == '134 West 29th Street - 2nd floor Studio 203') {
+		$row['address'] = '134 West 29th Street';
+		$row['notes'] = '2nd Floor Studio 203<br>' . $row['notes'];
+	} elseif ($row['city'] == 'Breezy Pt') {
+		$row['city'] = 'Queens';
+	} elseif ($row['address'] == 'Granite Springs Road') {
+		$row['address'] = '39 Granite Springs Rd';
+	} elseif ($row['location'] == 'North Woodmere Park Administration Building') {
+		$row['address'] = '750 Hungry Harbor Rd';
+	} elseif (strstr($row['address'], '65 East 89th Street')) {
+		$row['address'] = '65 East 89th Street';
+		$row['location'] = 'Church of Saint Thomas More';
+		$row['notes'] = 'Rectory Basement<br>' . $row['notes'];
+	} elseif ($row['address'] == '135 Foster Avenue') {
+		$row['location'] = 'Warwick United Methodist Church';
+		$row['address'] = '135 Forester Ave';
+	} elseif (strstr($row['address'], '22 Barclay Street')) {
+		$row['address'] = '22 Barclay Street';
+		$row['notes'] = 'Basement Chapel<br>' . $row['notes'];
+	} elseif ($row['location'] == 'Yorktown Grange Fair Building') {
+		$row['address'] = '99 Moseman Ave';
+		$row['city'] = 'Yorktown Heights';
+	} elseif ($row['address'] == 'Route 25A (By the Fish Hatchery)') {
+		$row['address'] = '1670 NY-25A (By the Fish Hatchery)';
+		$row['city'] = 'Cold Spring Harbor';
+	} elseif ($row['address'] == 'Old Main Street') {
+		$row['address'] = '1176 E Main St';
+	} elseif ($row['location'] == 'Central Christian Church') {
+		$row['address'] = '71 West St';
+	} elseif ($row['address'] == '73rd Street Betw 3rd & 4th Avenues, Basement') {
+		$row['address'] = '7320 4th Avenue';
+		$row['notes'] = 'Enter Betw 3rd & 4th Avenues, Basement<br>' . $row['notes'];
+	} elseif ($row['address'] == '25 East 15th- Conference Room H') {
+		$row['address'] = '25 East 15th Street';
+		$row['notes'] = 'Conference Room H<br>' . $row['notes'];
+	} elseif ($row['location'] == 'US Merchant Marine Academy') {
+		$row['address'] = '25300 Steamboat Road';
+		$row['city'] = 'Great Neck';
+		$row['notes'] = 'Mariners Chapel<br>Basement Lounge<br>' . $row['notes'];
+	} elseif ($row['location'] == 'On the beach at Broadway (In Fair Harbor)') {
+		$row['address'] = '315 Broadway';
+		$row['city'] = 'Saltaire';
+		$row['notes'] = 'On the beach at Broadway (In Fair Harbor)<br>' . $row['notes'];
+	} elseif ($row['address'] == '4 West 76th Street Meeting in the gym') {
+		$row['address'] = '4 West 76th Street';
+		$row['notes'] = 'Meeting in the gym<br>' . $row['notes'];
+	} elseif ($row['address'] == '546 East Boston Post Road- Mamaroneck') {
+		$row['address'] = '546 E Boston Post Rd';
+		$row['city'] = 'Mamaroneck';
+	} elseif ($row['address'] == '348 Beach 94th Street') {
+		$row['location'] = 'First Congregational Church';
+		$row['address'] = '320 Beach 94th St';
+		$row['city'] = 'Rockaway Beach';
+	} elseif ($row['city'] == 'Hollis') {
+		$row['city'] = 'Queens';
+	} elseif ($row['location'] == 'Tomkins Memorial Church') {
+		$row['location'] = 'Living Hope Fellowship';
+		$row['address'] = '326 Liberty Drive North';
+		$row['city'] = 'Tomkins Cove';
+		$row['notes'] = 'Across from Free Hill Road<br>' . $row['notes'];
+	}
+	
 	$row['day']		 = format_day($row['day']);
 	$row['name']	 = format_name($row['name']);
 	$row['location'] = format_location($row['location']);
 	$row['region']	 = format_region($row['region']);
+	$row['city']	 = format_city($row);
 	$row['state']	 = format_state($row['state'], $row['region']);
 	$row['country']	 = 'US';
 	$row['notes']	 = format_notes($row['notes']);
+	$row['types']	 = format_types($row['types']);
+	format_address($row);
+	if (empty($row['location'])) $row['location'] = $row['name'];
+	
 	$meetings[]		 = $row;
+}
+
+//delete all data and run import
+if (true) {
+	foreach ($meetings as &$meeting) $meeting = implode($tab, $meeting);		
+	array_unshift($meetings, implode($tab, $columns));
+	echo tsml_import(implode(PHP_EOL, $meetings), true);
+	do_action('admin_notices');
+	die('total time ' . (microtime(true) - $time_start) / 60); 
 }
 
 ?>
