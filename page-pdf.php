5293412ba85a8302ad631930ea662f3af915defb
Josh Reisner <josh@joshreisner.com>
2016-09-29 16:49:49 -1000

    progress on pdfs

--- page-pdf.php
+++ page-pdf.php
@@ -1,5 +1,13 @@
 <?php
 
+//pdf config
+$font_family			= 'helvetica';
+$font_size_header		= 18;
+$font_size				= 10;
+$starting_page_number	= 8;
+$orientation_even		= 'L';
+$orientation_odd		= 'R';
+
 //main sections are added manually to preserve book order. these must match the term_ids of the regions
 $regions = array(
 	1864 => array(), //Manhattan
@@ -178,72 +186,49 @@ unset($categories);
 
 require_once('vendor/tcpdf/tcpdf.php');
 
+class MyTCPDF extends TCPDF {
+
+    public function Header() {
+    }
+
+    public function Footer() {
+	    global $orientation_even, $orientation_odd;
+		$this->SetY(-15);
+		$orientation = ($this->getPage() % 2 == 0) ? $orientation_even : $orientation_odd;
+		$this->Cell(0, 10, $this->getAliasNumPage(), 0, false, $orientation, 0, '', 0, false, 'T', 'M');
+	}
+}
+
 //create new PDF
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, 'Letter', true, 'UTF-8', false);
-$pdf->SetCreator(PDF_CREATOR);
+$pdf = new MyTCPDF('P', 'mm', 'letter', true, 'UTF-8', false);
 $pdf->SetAuthor('New York Inter-Group');
 $pdf->SetTitle('Meeting List');
 
-// set default header data
-$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
-$pdf->setFooterData(array(0,64,0), array(0,64,128));
-
-// set header and footer fonts
-$pdf->setHeaderFont(array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-$pdf->setFooterFont(array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-
-// set default monospaced font
-$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-
-// set margins
-$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-
-// set auto page breaks
-$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-
-// set image scale factor
-$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-
-// set some language-dependent strings (optional)
-if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
-	require_once(dirname(__FILE__).'/lang/eng.php');
-	$pdf->setLanguagearray($l);
-}
-
-// ---------------------------------------------------------
-
-// set default font subsetting mode
+//fonts
+$pdf->SetFont($font_family, '', $font_size, '', true);
+$pdf->setHeaderFont(array($font_family, '', $font_size));
+$pdf->setFooterFont(array($font_family, '', $font_size));
 $pdf->setFontSubsetting(true);
 
-// Set font
-// dejavusans is a UTF-8 Unicode font, if you only need to
-// print standard ASCII chars, you can use core fonts like
-// helvetica or times to reduce file size.
-//$pdf->SetFont('dejavusans', '', 14, '', true);
+$pdf->setStartingPageNumber($starting_page_number);
 
-// Add a page
-// This method has several options, check the source code documentation for more information.
-$pdf->AddPage();
+//margins
+$pdf->SetMargins(15, 27, 15);
+$pdf->SetHeaderMargin(5);
+$pdf->SetFooterMargin(10);
+$pdf->SetAutoPageBreak(TRUE, 25);
 
-// set text shadow effect
-//$pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
+foreach ($regions as $region) {
+	$pdf->AddPage();
+	$pdf->setFont('', 'b', $font_size_header);
+	$pdf->Cell(0, 6, $region['name'], 0, 0, 'L', 0);	
+	$pdf->setFont('', 'r', $font_size);
+	if ($region['sub_regions']) {
+		foreach ($region['sub_regions'] as $sub_region => $rows) {
+			$pdf->Ln();
+			$pdf->Cell(0, 6, strtoupper($sub_region), 'TB', 0, 'L', 0);	
+		}
+	}
+}
 
-// Set some content to print
-$html = <<<EOD
-<h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
-<i>This is the first example of TCPDF library.</i>
-<p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
-<p>Please check the source code documentation and other examples for further information.</p>
-<p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>
-EOD;
-
-// Print text using writeHTMLCell()
-$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
-
-// ---------------------------------------------------------
-
-// Close and output PDF document
-// This method has several options, check the source code documentation for more information.
 $pdf->Output('meeting-list.pdf', 'I');
